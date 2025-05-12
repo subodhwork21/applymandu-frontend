4cd909cb6280d6a0f7d94caf11b31cd2bc701cbf
subodhach <subodh@flyingcode.com>
2025-05-12 21:10:33 +0545

    token added for verification

---
+++ app/verify-email/[token]/page.tsx
@@ -0,0 +1,44 @@
+'use client'
+import { toast } from "@/hooks/use-toast";
+import { baseFetcher } from "@/lib/fetcher";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const Page = () =>{
+    const {token} = useParams();
+    const router = useRouter();
+
+    useEffect(()=>{
+        const verifyEmail = async () => {
+            const {response, result, errors} = await baseFetcher("api/verify-email", {
+                method: "POST",
+            body: JSON.stringify({
+                token: token
+            })
+        });
+        if(response?.ok){
+            toast({
+                title: "Email verified successfully",
+                description: "Your email has been verified successfully",
+            });
+            router.push("/");
+        }
+        else{
+            toast({
+                title: "Error verifying email",
+                description: errors,
+            });
+            router.push("/");
+        }
+
+    }
+
+    verifyEmail();
+    }, [token])
+
+    return <div>
+        <h1>Verify Your Email</h1>
+    </div>
+}
+
+export default Page;
