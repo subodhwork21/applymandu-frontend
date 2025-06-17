7910864c8edc0aba2c4b92455fca0665ca15d875
subodhach <subodh@flyingcode.com>
2025-06-17 18:28:43 +0545

    Generate static params implemented on jobs

--- app/jobs/[slug]/layout.tsx
+++ app/jobs/[slug]/layout.tsx
@@ -1,38 +1,60 @@
 import { Metadata } from "next";
 
-
-
-
-export async function generateMetaData({ params }: { params: { slug: string } }): Promise<Metadata>{
-     const response = await fetch(process.env.NEXT_PUBLIC_API_URL + "api/seo/job-info/"+ params?.slug);
-    const data = await response.json();
-    return {
-        title: data?.title,
-        description: data?.description.slice(0, 155)+"...",
-        openGraph: {
-            images: [
-                {
-                    url: data?.image,
-                    width: 800,
-                    height: 600,
-                }
-            ]
-        }
+export async function generateStaticParams() {
+  try {
+    const response = await fetch(
+      process.env.NEXT_PUBLIC_API_URL + "api/seo/job-slugs"
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
     }
+    const data = await response.json();
+    return (
+      data?.job_slugs?.map((item: any) => ({
+        slug: item,
+      })) || []
+    );
+  } catch (error) {
+    return [];
+  }
 }
 
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata> {
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_API_URL + "api/seo/job-info/" + params?.slug
+  );
+  const data = await response.json();
+  return {
+    title: data?.title,
+    description: data?.description
+      ? data.description.length > 155
+        ? data.description.slice(0, 155) + "..."
+        : data.description
+      : "Job opportunity available",
+    openGraph: {
+      images: data?.image
+        ? [
+            {
+              url: data.image,
+              width: 800,
+              height: 600,
+            },
+          ]
+        : [],
+    },
+  };
+}
 
-
-
-
-export default async function Layout({children, params}:{
-    children: React.ReactNode, params:  {slug: string}
-}){
-
-  
-    return <>
-    {
-        children
-    }
-    </>
+export default async function Layout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { slug: string };
+}) {
+  return <>{children}</>;
 }
--- app/jobs/layout.tsx
+++ app/jobs/layout.tsx
@@ -1,9 +1,6 @@
 export default  function Layout({children}:{
     children: React.ReactNode
 }){
-
-
-
     return <>
     {
         children
